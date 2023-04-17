ec8f54b3351e4fee5d53341d934dc6b890823173
led <62636908+led-sol@users.noreply.github.com>
2023-04-17 22:13:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,28 +13,28 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-var con = mysql.createPool({
-  connectionLimit : 100,
+// var con = mysql.createPool({
+//   connectionLimit : 100,
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database:process.env.DB_DATABASE,
+//     debug    :  false
+//   });
+
+var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    debug    :  false
+    database:process.env.DB_DATABASE
   });
 
-// var con = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'password',
-//     database:'clients'
-//   });
 
-
-//  con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//     console.log('dd')
-//   });
+ con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    console.log('dd')
+  });
 
   app.get('/', (req, res) => { 
     res.send('ok')
